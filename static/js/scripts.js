9e64c4fde016219781d232cabfecf82112e71ace
daniels36999 <dyautibug.fie@unach.edu.ec>
2022-05-30 12:42:31 -0500

    Cambios

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,70 +1,45 @@
-﻿//https://www.eclipse.org/paho/clients/js/
-//document.getElementById("sensor1").innerHTML="LED1 ENCENDIDO";
+﻿﻿topicDaSottoscrivere = "daniels/test";
+messaggioDaInviare = "messaggio inviato via MQTT"
 
 // Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
-var client = new Paho.MQTT.Client("https://test.mosquitto.org/",8081, "web_" + parseInt(Math.random() * 100, 10));
+var client = new Paho.MQTT.Client("test.mosquitto.org", 8080, "myclientid_" + parseInt(Math.random() * 100, 10))
+//QUESTA E? QUELLA COPIATA CHE DOVFEBBE FUNZIONARE  ****************************** client = new Paho.MQTT.Client(location.hostname, Number(location.port), "clientId");
+//Example client = new Paho.MQTT.Client("m11.cloudmqtt.com", 32903, "web_" + parseInt(Math.random() * 100, 10));
 
-// Message recieved
 // set callback handlers
-  client.onConnectionLost = onConnectionLost;
-  client.onMessageArrived = onMessageArrived;
-  var options = {
-   useSSL: false,
-   userName: "",
-   password: "",
-    onSuccess:onConnect,
-    onFailure:doFail
-  }
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
 
-  // connect the client
-  client.connect(options);
-   
-  // called when the client connects
-  function onConnect() {
+// connect the client
+client.connect({ onSuccess: onConnect });
+
+
+// called when the client connects
+function onConnect(topicDaSottoscrivere, messaggioDaInviare) {
+
+
+
+    console.log("onconnect avviato")
     // Once a connection has been made, make a subscription and send a message.
-    console.log("Conectado...");
-	
-    client.subscribe("daniels/test");
-    message = new Paho.MQTT.Message("SE A CONECTADO A LA WEB EXITOSAMENTE");
-    message.destinationName = "dyautibug.fie@unach.edu.ec/test1";
+    console.log("onConnect");
+    client.subscribe(topicDaSottoscrivere);
+    message = new Paho.MQTT.Message(messaggioDaInviare);
+    console.log(messaggioDaInviare)
+    message.destinationName = "/World";
     client.send(message);
-	
-  }
+}
 
-  function doFail(e){
-    console.log(e);
-	
-  }
-
-  // called when the client loses its connection
-  function onConnectionLost(responseObject) {
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:"+responseObject.errorMessage);
+        console.log("onConnectionLost:" + responseObject.errorMessage);
     }
-  }
-ResTemp=0;
-ResPeso=0;
-ResAltura=0;
-  // called when a message arrives
-  function onMessageArrived(message) {
-    //console.log("onMessageArrived:"+message.payloadString);
-	  //document.getElementById("sensor").innerHTML=message.payloadString;
-	sms=(message.payloadString);
-	console.log(sms); 
-	ResTemp=sms;
-	VectorDatos=sms.split(";")  
-	Npalabras =VectorDatos.length;
-	ResTemp=VectorDatos[0];
-	ResPeso=VectorDatos[1];
-	ResAltura=VectorDatos[2];
-	  
-	  if(Npalabras>=4&&var1=="MM"){
-	  }
-	  if(var1=="OO"){
-	  }
-	
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    console.log("onMessageArrived:" + message.payloadString);
+}
 	  
 //	Dividir = sms.split(" ");
 //	Npalabras =Dividir.length;
